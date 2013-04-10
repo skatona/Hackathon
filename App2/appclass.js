bb0fc6037ab876e43b17bbd9f1d479a5e73e8aa0
Shaun Katona <shaun.katona@markit.com>
2013-04-10 16:59:17 -0600

    initial commit

---
+++ App2/appclass.js
@@ -0,0 +1,15 @@
+F2.Apps["com_rbc_hackathon2"] = (function() {
+    var App_Class = function(appConfig, appContent, root) {
+    	// constructor
+    	this.appConfig = appConfig;
+    	this.appContent = appContent;
+    	this.$root = $(root); //if you're using jQuery.
+    }
+
+    App_Class.prototype.init = function() {
+        // perform init actions
+        F2.log("Init called.")
+    }
+
+    return App_Class;
+})();
