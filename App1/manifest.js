ea93a45125f7d3f94ccdcdf1425fe0e1e84c0d9c
Shaun Katona <shaun.katona@markit.com>
2013-04-10 16:43:30 -0600

    initial commit

---
+++ App1/manifest.js
@@ -0,0 +1,12 @@
+F2_jsonpCallback_com_rbc_hackathon1({
+	"scripts":[
+		"appclass.js"
+	],	 
+	"styles":[
+		"app.css"
+	],	 
+	"apps":[{
+		"data": {},
+		"html":"<div><p class=\"hi\">Hello, world. I'm RBC App 1.</p></div>"
+	}]
+})
---
+++ App2/manifest.js
@@ -0,0 +1,12 @@
+F2_jsonpCallback_com_rbc_hackathon2({
+	"scripts":[
+		"appclass.js"
+	],	 
+	"styles":[
+		"app.css"
+	],	 
+	"apps":[{
+		"data": {},
+		"html":"<div><p class=\"hi\">Hello, world. I'm RBC App 2.</p></div>"
+	}]
+})
